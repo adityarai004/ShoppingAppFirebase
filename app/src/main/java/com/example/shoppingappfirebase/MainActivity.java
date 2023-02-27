7755fe3df98bfbb820e2b07670d02c12535e8075
Aditya <adityaadiirai.004@gmail.com>
2023-02-27 20:50:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/shoppingappfirebase/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.shoppingappfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("GoldPrice").child("Price");
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.setValue(editText.getText().toString());
+            }
+        });
+
+        mDatabase.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+}
